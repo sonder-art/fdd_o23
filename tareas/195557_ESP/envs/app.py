0e5bef519fd03b92692aa7f5d9fb1d61fb4ea94f
esandovalp <100039561+esandovalp@users.noreply.github.com>
2023-09-27 18:46:10 -0600

    changed app.py

--- tareas/195557_ESP/envs/app.py
+++ tareas/195557_ESP/envs/app.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     print('Ejecutando Codigo')
     import pandas as pd
-    from ydata_profiling import ProfileReport
+    import os
+    os.system('pip install ydata-profiling')
     print('Todo se realizo con exito')
